69663b8e7c97a7ccffdff7c8e9758bd03657e0e0
myonX <ri.myonsan.x@gmail.com>
2026-05-18 18:04:17 +0900

    処理系のかれんとに関係泣く動作するようにした

--- tests/main.lisp
+++ tests/main.lisp
@@ -8,16 +8,16 @@
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-nbt)' in your Lisp.
 
 (defun equal-binary-file (path-1 path-2)
-  (with-open-file (path-1-in path-1 :direction :input :element-type '(unsigned-byte 8))
-                  (with-open-file (path-2-in path-2 :direction :input :element-type '(unsigned-byte 8))
-                  (let ((path-1-list (alexandria:read-file-into-byte-vector path-1-in))
-                        (path-2-list (alexandria:read-file-into-byte-vector path-2-in)))
-                       (equal path-1-list path-2-list)))))
+  (equalp (alexandria:read-file-into-byte-vector (xxx->cl-nbt/xxx path-1))
+          (alexandria:read-file-into-byte-vector (xxx->cl-nbt/xxx path-2))))
+
+(defun xxx->cl-nbt/xxx (relative-path)
+  (uiop:subpathname (asdf:system-source-directory :cl-nbt) relative-path))
 
 (deftest tag-test
          (testing "number-tags-test"
                   (let ((expected-file-path "tests/test-file/number-test.nbt") (operator-output-path "tests/output/number-result.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* operator-output-path :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
+                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
                                   (tag-compound "" ((tag-byte "byte" 1)
                                                     (tag-short "short" 1)
                                                     (tag-integer "integer" 1)
@@ -28,16 +28,16 @@
                     (ok (equal-binary-file expected-file-path operator-output-path))))
          (testing "list-tag-test"
                   (let ((expected-file-path "tests/test-file/list-test.nbt") (operator-output-path "tests/output/list-result.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* operator-output-path :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
+                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
                                   (tag-compound "" ((tag-list "byte-list" (byte 1 2 3)))))
                     (ok (equal-binary-file expected-file-path operator-output-path))))
          (testing "list-list-test"
                   (let ((expected-file-path "tests/test-file/list-list-test.nbt") (operator-output-path "tests/output/list-list-test.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* operator-output-path :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
+                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
                                   (tag-compound "" ((tag-list "list-list" (list (byte 1 2 3) (byte 4 5 6))))))
                     (ok (equal-binary-file expected-file-path operator-output-path))))
          (testing "void-list-test"
                   (let ((expected-file-path "tests/test-file/void-list-test.nbt") (operator-output-path "tests/output/void-list-test.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* operator-output-path :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
+                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
                                   (tag-compound "" ((tag-list "void-list" (byte)))))
                     (ok (equal-binary-file expected-file-path operator-output-path)))))
