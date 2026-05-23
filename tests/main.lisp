8f4304b6f5c757d4822d587fd0d07392e587d06e
myonX <ri.myonsan.x@gmail.com>
2026-05-23 20:56:43 +0900

    wrappped rove

--- tests/main.lisp
+++ tests/main.lisp
@@ -14,35 +14,41 @@
 (defun xxx->cl-nbt/xxx (relative-path)
   (uiop:subpathname (asdf:system-source-directory :cl-nbt) relative-path))
 
-(deftest tag-test
-         (testing "number-tags-test"
-                  (let ((expected-file-path "tests/test-file/number-test.nbt") (operator-output-path "tests/output/number-result.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
-                                  (tag-compound "" ((tag-byte "byte" 1)
-                                                    (tag-short "short" 1)
-                                                    (tag-integer "integer" 1)
-                                                    (tag-long "long" 1)
-                                                    (tag-float "float" 1.0)
-                                                    (tag-double "double" 1.0d0)
-                                                    (tag-string "string" "string"))))
-                    (ok (equal-binary-file expected-file-path operator-output-path))))
-         (testing "list-tag-test"
-                  (let ((expected-file-path "tests/test-file/list-test.nbt") (operator-output-path "tests/output/list-result.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
-                                  (tag-compound "" ((tag-list "byte-list" (byte 1 2 3)))))
-                    (ok (equal-binary-file expected-file-path operator-output-path))))
-         (testing "list-list-test"
-                  (let ((expected-file-path "tests/test-file/list-list-test.nbt") (operator-output-path "tests/output/list-list-test.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
-                                  (tag-compound "" ((tag-list "list-list" (list (byte 1 2 3) (byte 4 5 6))))))
-                    (ok (equal-binary-file expected-file-path operator-output-path))))
-         (testing "void-list-test"
-                  (let ((expected-file-path "tests/test-file/void-list-test.nbt") (operator-output-path "tests/output/void-list-test.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
-                                  (tag-compound "" ((tag-list "void-list" (byte)))))
-                    (ok (equal-binary-file expected-file-path operator-output-path))))
-         (testing "compound-list-test"
-                  (let ((expected-file-path "tests/test-file/compound-list.nbt") (operator-output-path "tests/output/compound-list-test.nbt"))
-                    (with-open-file (cl-nbt::*nbt-output* (xxx->cl-nbt/xxx operator-output-path) :direction :output :element-type '(unsigned-byte 8) :if-exists :supersede :if-does-not-exist :create)
-                                  (tag-compound "" ((tag-list "compound-list" (compound ((tag-byte "byte" 1)) ((tag-integer "integer" 1)))))))
-                    (ok (equal-binary-file expected-file-path operator-output-path)))))
+(defmacro tag-testing (testing-word expected-file-path operator-output-path tag)
+  `(testing ,testing-word
+     (progn (cl-nbt:serialize-tags (xxx->cl-nbt/xxx ,operator-output-path)
+                                   ',tag)
+            (ok (equal-binary-file ,expected-file-path ,operator-output-path)))))
+
+(defmacro def-tag-test (test-name &rest rest)
+  `(deftest ,(quote test-name) ,@(mapcar (lambda (element) (macroexpand-1 `(tag-testing ,@element))) rest)))
+
+(def-tag-test tag-test
+  ("nubmer-tags-test"
+   "tests/test-file/number-test.nbt"
+   "tests/output/number-result.nbt"
+   (tag-compound "" ((tag-byte "byte" 1)
+                     (tag-short "short" 1)
+                     (tag-integer "integer" 1)
+                     (tag-long "long" 1)
+                     (tag-float "float" 1.0)
+                     (tag-double "double" 1.0d0)
+                     (tag-string "string" "string"))))
+  ("list-tag-test"
+   "tests/test-file/list-test.nbt"
+   "tests/output/list-result.nbt"
+   (tag-compound "" ((tag-list "byte-list" (byte 1 2 3)))))
+  ("list-list-test"
+   "tests/test-file/list-list-test.nbt"
+   "tests/output/list-list-test.nbt"
+   (tag-compound "" ((tag-list "list-list" (list (byte 1 2 3) (byte 4 5 6))))))
+  ("void-list-test"
+   "tests/test-file/void-list-test.nbt"
+   "tests/output/void-list-test.nbt"
+   (tag-compound "" ((tag-list "void-list" (byte)))))
+  ("compound-list-test"
+   "tests/test-file/compound-list.nbt"
+   "tests/output/compound-list-test.nbt"
+   (tag-compound "" ((tag-list "compound-list"
+                               (compound ((tag-byte "byte" 1))
+                                         ((tag-integer "integer" 1))))))))
