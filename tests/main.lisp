673cb3128c2508c1eaf6b7b56a6008c4fcb4a352
myonX <ri.myonsan.x@gmail.com>
2026-08-11 08:04:36 +0900

    add tests

--- tests/main.lisp
+++ tests/main.lisp
@@ -51,4 +51,22 @@
     (ok (equalp *compound-list-test-expected*
                 (tag->vector (tag-compound "" (list (tag-list "compound-list"
                                                               (list 'compound (list (tag-byte "byte" 1))
-                                                                        (list (tag-integer "integer" 1)))))))))))
+                                                                    (list (tag-integer "integer" 1)))))))))))
+;
+(deftest write-nbt
+  (testing "number-tags-test"
+    (ok (equalp '(tag-compound "" (list (tag-byte "byte" 1)(tag-short "short" 1)(tag-integer "integer" 1)(tag-long "long" 1)(tag-float "float" 1.0)(tag-double "double" 1.0d0)(tag-string "string" "string")))
+                (parse-tags *number-test-expected*))))
+  (testing "list-tsg-test"
+    (ok (equalp (parse-tags *list-test-expected*) '(tag-compound "" (list (tag-list "byte-list" '(byte 1 2 3)))))))
+  (testing "list-list-test"
+    (ok (equalp  (parse-tags *list-list-test-expected*)
+                '(tag-compound "" (list (tag-list "list-list" '(list (byte 1 2 3) (byte 4 5 6))))))))
+  (testing "void-list-test"
+    (ok (equalp  (parse-tags *void-list-test-expected*)
+                '(tag-compound "" (list (tag-list "void-list" '(byte)))))))
+  (testing "compound-list-test"
+    (ok (equalp  (parse-tags *compound-list-test-expected*)
+                '(tag-compound "" (list (tag-list "compound-list"
+                                                              (list 'compound (list (tag-byte "byte" 1))
+                                                                    (list (tag-integer "integer" 1))))))))))
